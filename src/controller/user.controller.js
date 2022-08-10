47a70956683c6935dbbb14a232c6ab97f5745efb
Pedro Fonseca <pedrinhopedro01@gmail.com>
2022-08-10 19:13:15 -0300

    🧹 chore: adicionado arquivos para correção do linter

---
+++ src/controller/user.controller.js
@@ -0,0 +1,25 @@
+const userServices = require('../services/user.services');
+
+const getAllUsers = async (_req, res) => {
+  try {
+    const users = await userServices.getAllUsers();
+    return res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+const createUser = async (req, res) => {
+  try {
+    const { displayName, email, password, image } = req.body;
+    const user = await userServices.createUser({ displayName, email, password, image });
+
+    if (!user) return res.status(409).json({ message: 'User already registered' });
+
+    res.status(201).json({ user });
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message });
+  }
+};
+
+module.exports = { getAllUsers, createUser };
--- src/services/auth.services.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-const jwtService = require('./jwt.services');
-const passwordService = require('./password.services');
-
-const authMiddleware = require('../middleware/authMiddleware');
-
-const { User } = require('../database/models');
